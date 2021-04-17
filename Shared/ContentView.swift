1341d879ad555ec2e287936d16d2f48e7b208ce8
alexis <alexis@wildapricot.com>
2021-04-17 14:34:55 +0300

    setup proj

---
+++ Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Alexis Grigorev on 17.04.2021.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/swiftui_classic_tabview_customisationApp.swift
@@ -0,0 +1,18 @@
+//
+//  swiftui_classic_tabview_customisationApp.swift
+//  Shared
+//
+//  Created by Alexis Grigorev on 17.04.2021.
+//
+//
+
+import SwiftUI
+
+@main
+struct swiftui_classic_tabview_customisationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
